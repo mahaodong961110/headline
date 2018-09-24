4571ccca29f0b4f73335aed2bd605fba3c469099
mhd961110 <17835424121@163.com>
2018-09-24 16:19:02 +0800

    ..

--- controller/wechat/wechat.php
+++ controller/wechat/wechat.php
@@ -1,25 +1,86 @@
 <?php
 include "../core/db.php";
+function https_request($url,$data=""){
+  // 开启curl
+  $ch=curl_init();
+  // 设置传输选项
+  // 设置传输地址
+  curl_setopt($ch, CURLOPT_URL, $url);
+  // 以文件流的形式返回
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  /**/
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //不验证证书下同
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+
+  if ($data) {
+    // 以post方式
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+  }
+
+  $headers = array("Content-type: application/json;charset=UTF-8","Accept: application/json","Cache-Control: no-cache", "Pragma: no-cache");
+
+  curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+
+  // 发送curl
+  $request=curl_exec($ch);
+  $tmpArr=json_decode($request,TRUE);
+  if (is_array($tmpArr)) {
+    return $tmpArr;
+  }else{
+    return $request;
+  }
+  // 关闭资源
+  curl_close($ch);
+}
 //数据资源
 class wechat extends db
 {
+// 	//查询对应的Openid的数据
+	 public function users_feed()
+	 {
+	 	$openid = $_GET['openid'];
+	 	$page = (int)$_GET['page'];
+	 	$sql = 'select * from feeds where openid = "'.$openid.'" order by ctime desc limit 10 offset ' . ($page-1) * 10;
+		$users_feed = $this->pdo->query($sql)->fetchAll();
+		echo json_encode($users_feed);
+	 }
+	 //查询所有数据
 	 public function feed()
 	 {
-	 	$array = $this->pdo
-     		->query('select * from feeds')
+        $page = (int)$_GET['page'];
+	 	$feeds = $this->pdo
+     		->query('select * from feeds order by ctime desc limit 3 offset ' . ($page-1) * 3 )
      		->fetchAll();
-     	echo json_encode($array);
+     	echo json_encode($feeds);
 	 }
-
+//行为资源。插入数据库
 	public function insert()
     {
-        $stmt = $this->pdo->prepare("insert into feeds(user_avatar,user_name,content,images,publish_time,publish_address)values(?,?,?,?,?,?)");
-        $stmt->bindValue(1,$_GET['user_avatar']);
-        $stmt->bindValue(2,$_GET['user_name']);
-        $stmt->bindValue(3,$_GET['content']);
-        $stmt->bindValue(4,'');
-        $stmt->bindValue(5,'12分钟前');
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wx648f891ef721c399&secret=0127742ee6712a12ea408d00b0c20fc8&js_code='.$_GET['code'].'&grant_type=authorization_code';
+        $result = https_request($url);
+        $openid = $result['openid'];
+        print_r($result);
+        $stmt = $this->pdo->prepare("insert into feeds(openid,user_avatar,user_name,content,images,publish_address)values(?,?,?,?,?,?)");
+        $stmt->bindValue(1,$openid);
+        $stmt->bindValue(2,$_GET['user_avatar']);
+        $stmt->bindValue(3,$_GET['user_name']);
+        $stmt->bindValue(4,$_GET['content']);
+        $stmt->bindValue(5,$_GET['images']);
         $stmt->bindValue(6,'悉尼');
-        echo $stmt->execute();
+        $stmt->execute();
+        echo $openid;
     }
-}
+
+    //行为资源 你给我图片 我给你存储，并且给你资源名。
+    public function upload()
+    {
+       $src = $_FILES['f']['tmp_name'];
+       $file_name = $_FILES['f']['name'];
+       $dist = './assets/wechat/' .$file_name ;
+       move_uploaded_file($src, $dist);
+       echo 'http://try.third.com/assets/wechat/'.$file_name;
+    }
+} 
+
--- database/feeds.sql
+++ database/feeds.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2018-09-12 08:43:22
+-- Generation Time: 2018-09-24 10:16:13
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -28,29 +28,41 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `feeds` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(255) NOT NULL,
   `user_avatar` varchar(255) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `content` varchar(255) NOT NULL,
-  `images` varchar(255) NOT NULL,
-  `publish_time` varchar(255) NOT NULL,
+  `images` text NOT NULL,
   `publish_address` varchar(255) NOT NULL,
+  `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
 
 --
 -- 转存表中的数据 `feeds`
 --
 
-INSERT INTO `feeds` (`id`, `user_avatar`, `user_name`, `content`, `images`, `publish_time`, `publish_address`) VALUES
-(1, 'http://img2.woyaogexing.com/2018/09/10/bc867a2c5dd84b8abe83cd20f95b2280!400x400.jpeg', '欣玥', '最近科技圈最火的手机，应该就属vivo X23了。', 'http://img13.360buyimg.com/n7/jfs/t20293/89/697940413/175156/2dc4fbd1/5b15ffd5Nccf91838.jpg;http://img13.360buyimg.com/n7/jfs/t20293/89/697940413/175156/2dc4fbd1/5b15ffd5Nccf91838.jpg', '8分钟前', '墨尔本'),
-(2, 'http://img2.woyaogexing.com/2018/09/10/bc867a2c5dd84b8abe83cd20f95b2280!400x400.jpeg', '欣玥', '最近科技圈最火的手机，应该就属vivo X23了。', 'https://article-fd.zol-img.com.cn/t_s200x150/g5/M00/0A/0A/ChMkJluM_M-IOxrPAAESq-GbdG4AArbJQO60kEAARLD446.jpg;https://article-fd.zol-img.com.cn/t_s200x150/g5/M00/0A/0A/ChMkJluM_M-IOxrPAAESq-GbdG4AArbJQO60kEAARLD446.jpg', '8分钟前', '墨尔本'),
-(3, 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic12pYlMicItW8U80b3dZibPNQA/132', '马浩东', '9月你好', '', '12分钟前', '悉尼'),
-(4, 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic12pYlMicItW8U80b3dZibPNQA/132', '马浩东', '未来可期', '', '12分钟前', '悉尼'),
-(5, 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic12pYlMicItW8U80b3dZibPNQA/132', '马浩东', '三观要正', '', '12分钟前', '悉尼'),
-(6, 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic12pYlMicItW8U80b3dZibPNQA/132', '马浩东', 'My   Love', '', '12分钟前', '悉尼'),
-(7, 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic12pYlMicItW8U80b3dZibPNQA/132', '马浩东', '最近科技圈最火的手机，应该就属vivo X23了。', '', '12分钟前', '悉尼'),
-(8, 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic12pYlMicItW8U80b3dZibPNQA/132', '马浩东', '最近科技圈最火的手机，应该就属vivo X23了。', '', '12分钟前', '悉尼'),
-(9, 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic12pYlMicItW8U80b3dZibPNQA/132', '马浩东', '最近科技圈最火的手机，应该就属vivo X23了。', '', '12分钟前', '悉尼');
+INSERT INTO `feeds` (`id`, `openid`, `user_avatar`, `user_name`, `content`, `images`, `publish_address`, `ctime`) VALUES
+(1, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'try first oppenid', 'http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.kEkDEH73TpGha917b647f30af0e0f8523196dbfd4ea7.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.TyAjencwxIoQ9bf5ee5deacdf9b48725cfc74b47b168.jpg', '悉尼', '2018-09-23 13:22:20'),
+(2, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', '1', 'http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.a14zFwo2O2Nya917b647f30af0e0f8523196dbfd4ea7.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.4OcjgW3Rr0WTa917b647f30af0e0f8523196dbfd4ea7.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.a9no9QFSLG7B40e2685f9f624c5e4b30ee62ac40d47b.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.VhuKddrpApMw70acffff8381569fcd7e388d578c5116.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.wUgPOO6gz4bH07d4fd23b9721f17967766b187eccbe6.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.EIW4GZpzrB0R07d4fd23b9721f17967766b187eccbe6.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.BbVlRAVmN6PU9bf5ee5deacdf9b48725cfc74b47b168.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.LsCRWENHLi4M9bf5ee5deacdf9b48725cfc74b47b168.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.KkKXpCghC1Xa9bf5ee5deacdf9b48725cfc74b47b168.jpg', '悉尼', '2018-09-23 13:27:35'),
+(3, 'opZhN5SZ6bmfJnP6OB18pjf9QpJo', 'https://wx.qlogo.cn/mmopen/vi_32/zycMugKptp0jKhKJswXmJQAWJRynlUWuRYzSbtfmdTZWgJib81NABrnnibGricWuTaiaU782SHVnsDOdFEzlsXCwCA/132', '渡。', '2', 'http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs82gmmeRoiv2cwCU8TR2oGk.Mi8HeJ6i1MVD40e2685f9f624c5e4b30ee62ac40d47b.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs82gmmeRoiv2cwCU8TR2oGk.km34RA4TEbnoa917b647f30af0e0f8523196dbfd4ea7.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs82gmmeRoiv2cwCU8TR2oGk.9TTGXvseFkwZ70acffff8381569fcd7e388d578c5116.jpg', '悉尼', '2018-09-23 13:43:26'),
+(4, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', '123', '', '悉尼', '2018-09-23 14:23:34'),
+(5, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'uhuii', '', '悉尼', '2018-09-23 14:44:50'),
+(6, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', '789', '', '悉尼', '2018-09-23 15:02:09'),
+(7, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'mahaodng ', '', '悉尼', '2018-09-23 15:32:30'),
+(8, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', '123', '', '悉尼', '2018-09-23 15:58:44'),
+(9, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'qweqwe', '', '悉尼', '2018-09-23 16:07:41'),
+(10, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', '432423423ewrwe', '', '悉尼', '2018-09-23 16:08:02'),
+(11, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'qweqwe', 'http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs-M5KU0V-0AqWovUQH2p3p0.3HirdJ9Qa5Q440e2685f9f624c5e4b30ee62ac40d47b.jpg', '悉尼', '2018-09-23 16:30:00'),
+(12, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', '纤维', '', '悉尼', '2018-09-24 07:12:00'),
+(13, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', '1', '', '悉尼', '2018-09-24 07:13:53'),
+(14, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'we', '', '悉尼', '2018-09-24 07:26:00'),
+(15, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'www', '', '悉尼', '2018-09-24 07:29:01'),
+(16, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'eee', '', '悉尼', '2018-09-24 07:48:43'),
+(17, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'qweqwe', '', '悉尼', '2018-09-24 07:57:05'),
+(18, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'weqweqweqw', '', '悉尼', '2018-09-24 07:58:32'),
+(19, 'opZhN5etBG7WXh5hVd7CN4w7v7as', 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erxMnXnb4zE8dwdtTSCOqGicD3rxwGHzIjEWf2JPVgY9RDDP5AQrLaic1vGahyTYXMslwt0Fl4jf60w/132', '马浩东', 'qwe', '', '悉尼', '2018-09-24 08:01:04'),
+(20, 'opZhN5SZ6bmfJnP6OB18pjf9QpJo', 'https://wx.qlogo.cn/mmopen/vi_32/zycMugKptp0jKhKJswXmJQAWJRynlUWuRYzSbtfmdTZWgJib81NABrnnibGricWuTaiaU782SHVnsDOdFEzlsXCwCA/132', '渡。', '123', 'http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs82gmmeRoiv2cwCU8TR2oGk.tRZCaZS5KXUXa917b647f30af0e0f8523196dbfd4ea7.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs82gmmeRoiv2cwCU8TR2oGk.RHOgGQwO7eg140e2685f9f624c5e4b30ee62ac40d47b.jpg;http://try.third.com/assets/wechat/wx648f891ef721c399.o6zAJs82gmmeRoiv2cwCU8TR2oGk.suctxccXeP8S70acffff8381569fcd7e388d578c5116.jpg', '悉尼', '2018-09-24 08:07:46');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
